9a4ef02c0cb33c78ed744c4833e1ede23f22f87d
pomorange <nemanjapipi@hotmail.com>
2019-04-15 19:31:04 +0200

    zavrsio i drugu formu

--- Airline Reservations/PessengerLookup.Designer.cs
+++ Airline Reservations/PessengerLookup.Designer.cs
@@ -28,19 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dgvPassengers = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seatRowDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seatColumnDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isOnWaitingListDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.passengerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.passengerBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.passengerBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPassengers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passengerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passengerBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passengerBindingSource2)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvPassengers
             // 
+            this.dgvPassengers.AutoGenerateColumns = false;
             this.dgvPassengers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPassengers.Location = new System.Drawing.Point(12, 29);
+            this.dgvPassengers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.seatRowDataGridViewTextBoxColumn,
+            this.seatColumnDataGridViewTextBoxColumn,
+            this.isOnWaitingListDataGridViewCheckBoxColumn});
+            this.dgvPassengers.DataSource = this.passengerBindingSource;
+            this.dgvPassengers.Location = new System.Drawing.Point(15, 29);
             this.dgvPassengers.Name = "dgvPassengers";
-            this.dgvPassengers.Size = new System.Drawing.Size(378, 150);
+            this.dgvPassengers.Size = new System.Drawing.Size(343, 150);
             this.dgvPassengers.TabIndex = 0;
+            this.dgvPassengers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPassengers_CellContentClick);
             // 
             // label1
             // 
@@ -53,24 +72,65 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(289, 185);
+            this.btnCancel.Location = new System.Drawing.Point(257, 185);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(101, 23);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // seatRowDataGridViewTextBoxColumn
+            // 
+            this.seatRowDataGridViewTextBoxColumn.DataPropertyName = "SeatRow";
+            this.seatRowDataGridViewTextBoxColumn.HeaderText = "SeatRow";
+            this.seatRowDataGridViewTextBoxColumn.Name = "seatRowDataGridViewTextBoxColumn";
+            // 
+            // seatColumnDataGridViewTextBoxColumn
+            // 
+            this.seatColumnDataGridViewTextBoxColumn.DataPropertyName = "SeatColumn";
+            this.seatColumnDataGridViewTextBoxColumn.HeaderText = "SeatColumn";
+            this.seatColumnDataGridViewTextBoxColumn.Name = "seatColumnDataGridViewTextBoxColumn";
+            // 
+            // isOnWaitingListDataGridViewCheckBoxColumn
+            // 
+            this.isOnWaitingListDataGridViewCheckBoxColumn.DataPropertyName = "IsOnWaitingList";
+            this.isOnWaitingListDataGridViewCheckBoxColumn.HeaderText = "IsOnWaitingList";
+            this.isOnWaitingListDataGridViewCheckBoxColumn.Name = "isOnWaitingListDataGridViewCheckBoxColumn";
+            // 
+            // passengerBindingSource
+            // 
+            this.passengerBindingSource.DataSource = typeof(Airline_Reservations.Passenger);
+            // 
+            // passengerBindingSource1
+            // 
+            this.passengerBindingSource1.DataSource = typeof(Airline_Reservations.Passenger);
+            // 
+            // passengerBindingSource2
+            // 
+            this.passengerBindingSource2.DataSource = typeof(Airline_Reservations.Passenger);
             // 
             // frmPassengerLookup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 229);
+            this.ClientSize = new System.Drawing.Size(387, 229);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvPassengers);
             this.Name = "frmPassengerLookup";
             this.Text = "PessengerLookup";
+            this.Load += new System.EventHandler(this.frmPassengerLookup_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPassengers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passengerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passengerBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passengerBindingSource2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,5 +141,12 @@
         private System.Windows.Forms.DataGridView dgvPassengers;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seatRowDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seatColumnDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isOnWaitingListDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.BindingSource passengerBindingSource;
+        private System.Windows.Forms.BindingSource passengerBindingSource1;
+        private System.Windows.Forms.BindingSource passengerBindingSource2;
     }
 }
--- Airline Reservations/PessengerLookup.cs
+++ Airline Reservations/PessengerLookup.cs
@@ -12,9 +12,46 @@ namespace Airline_Reservations
 {
     public partial class frmPassengerLookup : Form
     {
-        public frmPassengerLookup()
+        // Create passenger object and list of all passenger that are found based on a search from the main form
+        private List<Passenger> foundPassengers;
+        public Passenger passenger;
+
+        //create variable for mode (delete or view)
+        private string viewMode;
+
+        //create variable for cancellation, so no operation is performed
+        public bool isOperationCancelled;
+
+        public frmPassengerLookup(string viewMode)
         {
             InitializeComponent();
+            foundPassengers = frmReservations.passengers;
+            passenger = new Passenger();
+            isOperationCancelled = false;
+            this.viewMode = viewMode;
+        }
+        
+        //when the form load, sort the passengers by seat row number and column letter
+        private void frmPassengerLookup_Load(object sender, EventArgs e)
+        {
+            foundPassengers = foundPassengers.OrderBy(c => c.SeatColumn).OrderBy(r => r.SeatRow).ToList();
+            dgvPassengers.DataSource = foundPassengers;
+        }
+        //canel the operation, do not do anything just close the form
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            isOperationCancelled = true;
+            this.Close();
+        }
+
+        // when user clicks the name in the grid view bound it with passenger object 
+        private void dgvPassengers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(viewMode != "view")
+            {
+                passenger = (Passenger)dgvPassengers.CurrentRow.DataBoundItem;
+                Hide();
+            }
         }
     }
 }
