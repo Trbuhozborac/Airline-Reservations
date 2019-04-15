0e7dc302c8a4867870e5eeae4424869c5dc9d420
pomorange <nemanjapipi@hotmail.com>
2019-04-15 19:09:26 +0200

    zavrsio klasu passenger

--- Airline Reservations/Passenger.cs
+++ Airline Reservations/Passenger.cs
@@ -34,7 +34,62 @@ namespace Airline_Reservations
         }
 
         public Passenger() { }
-       
 
+        public bool IsValidPassenger(string name, string row, string column)
+        {
+            if (name.Trim().Equals(""))
+            {
+                ErrorMessage = "Name is Required!";
+                return false;
+            }
+
+            int validRow;
+            if (!int.TryParse(row, out validRow) || validRow < 1 || validRow > 10)
+            {
+                ErrorMessage = "Valid Seat Row is Required!";
+                return false;
+            }
+
+            if (column.ToUpper() != "A" && column.ToUpper() != "B" && column.ToUpper() != "C" && column.ToUpper() != "D")
+            {
+                ErrorMessage = "Valid Seat Column is Required!";
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IsSeatTaken(string seatRow, string seatColumn)
+        {
+            var isTaken = frmReservations.passengers.Where(p => p.SeatRow == Convert.ToInt32(seatRow) &&
+                                                                p.SeatColumn.ToUpper() == seatColumn.ToUpper()).Count();
+
+            return isTaken == 0 ? false : true;
+        }
+
+        public List<Passenger> GetPassengersByName(string name)
+        {
+            return frmReservations.passengers.Where(p => p.Name == name).ToList();
+        }
+
+        public void AddPassenger(Passenger passenger)
+        {
+            frmReservations.passengers.Add(passenger);
+        }
+
+        public void RemovePassenger(Passenger passenger)
+        {
+            frmReservations.passengers.Remove(passenger);
+        }
+
+        public int NumberOfSeatedPassengers()
+        {
+            return frmReservations.passengers.Where(p => p.IsOnWaitingList == false).Count();
+        }
+
+        public int NumberOfWaitingPassengers()
+        {
+            return frmReservations.passengers.Where(p => p.IsOnWaitingList == true).Count();
+        }
     }
 }
